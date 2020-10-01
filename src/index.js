c157018464acd522648c0c45f65f0b0259075514
Sergey Durov <sergssd@yandex.com>
2020-10-01 17:05:10 +0500

    feat: add all tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof array != 'object' || array.length == 0) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array != 'object' || array.length == 0) return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof array != 'object' || array.length == 0) return 0;
+  let sum = array.reduce((res, item) => {
+    return res += item
+  }, 0);
+  return sum/array.length;
 }
